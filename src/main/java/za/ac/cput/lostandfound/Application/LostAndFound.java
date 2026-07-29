2f0538a13d91ec9f82be85519dbd4b060163b45e
rjbur <rjbur@192.168.1.136>
2026-07-29 21:10:47 +0200

    Lost and found Commit

---
+++ src/main/java/za/ac/cput/lostandfound/Application/LostAndFound.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.ac.cput.lostandfound.Application;
+
+/**
+ *
+ * @author rjbur
+ */
+public class LostAndFound {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
